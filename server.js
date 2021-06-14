95b9cd7e1a50b5255369abd5cd032adf72e1ec93
Cat <morancathy@gmail.com>
2021-06-14 18:33:38 -0500

    I can view a list of all my pokemon in the browser

--- server.js
+++ server.js
@@ -1,24 +1,50 @@
+// Load express, create express app, set PORT, create 'require' consts
 const express = require('express');
 const app = express();
 const PORT = 3000;
 const pokemon = require('./models/pokemon')
 
+// Configure the app (app.set)
 app.set('view engine', 'jsx');                          //MUST be above the Routes
 app.engine('jsx', require('express-react-views').createEngine())
 
+// Mount middleware (app.use)
+app.use(express.urlencoded({ extended: true }))
+
+/*****************
+Mount INDUCES Routes
+******************/
+/*
+Index
+*/
 app.get('/', (req, res) => {
   res.send('<h1>Welcome to the Pokemon App!</h1>')
 })
 
-app.get('/pokemon', (req, res) => {
-  res.render('Index');
+app.get('/pokemon/', (req, res) => {
+  res.render('Index', {pokemon: pokemon});
 })
+/*
+New
+*/
+/*
+Delete
+*/
+/*
+Update
+*/
+/*
+Create
+*/
+/*
+Edit
+*/
+/*
+Show
+*/
 
 
-
-
-
-
+//tell app to listen on port 3000
 app.listen(PORT, () => {
   console.log(`Listening in on ${PORT}`);
 })
--- views/Index.jsx
+++ views/Index.jsx
@@ -5,12 +5,23 @@ color: '#ffffff',
 backgroundColor: '#FF69B4',
 };
 
+
 class Index extends React.Component {
   render(){
-    //const
+    const pokemon = this.props.pokemon;
     return (
       <div style={myStyle}>
         <h1>See All The Pokemon!</h1>
+        <ul>
+          {pokemon.map((charactor, i) =>{
+              return(
+                <li>
+                  Hi, I am {(pokemon[i].name.charAt(0).toUpperCase() + pokemon[i].name.slice(1))}!
+                </li>
+              )
+            })
+          }
+        </ul>
       </div>
     )
   }
