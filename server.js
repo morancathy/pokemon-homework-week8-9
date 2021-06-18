f17b4bea8ae6475eea5d51f5d18c857c2de1cb1e
Cat <morancathy@gmail.com>
2021-06-17 21:34:42 -0500

    updating code for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const Pokemon = require('./models/pokemon.js');
 
 // Database set up
